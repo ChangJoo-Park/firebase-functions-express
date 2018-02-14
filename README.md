bf8c2b6eb0eb8c203c407ada0bd93d2ae2a58cdc
ChangJoo Park <pcjpcj2@gmail.com>
2018-02-14 17:32:20 +0900

    Add index and not-found page and related routes

---
+++ README.md
@@ -0,0 +1,10 @@
+# Firebase functions + Express
+
+This repo is for testing google firebase cloud functions.
+Show reservation confirmed page (using reservation id)
+
+## Installation
+
+## Development
+
+## Deploy
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const express = require("express");
+const cors = require("cors")({
+  origin: true
+});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!")
-})
+const app = express();
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').renderFile);
+app.use(cors);
+app.use(express.static('public'));
+
+// Routes
+const infoRoute = (req, res) => {
+  const reservation_id = req.params['reservation_id']
+  // TODO: Request reservation with reservation_id,
+  // if found reservation, render index.
+  // if not - found or reservation_id is nil, redirect to not-found
+  const found = true
+  if (found && reservation_id === '0000') {
+    res.render("index", { id: reservation_id });
+  }
+  res.redirect('/app/not-found')
+}
+
+const notFoundRoute = (req, res) => {
+  res.render('not-found')
+}
+
+app.get("/reservations/:reservation_id", infoRoute);
+app.get('/not-found', notFoundRoute)
+app.get('*', (req, res) => { res.redirect('/app/not-found') })
+
+exports.app = functions.https.onRequest(app);
